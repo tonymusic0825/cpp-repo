ec8c5029797e82ed4b0c128158a452ec4eaccace
tonymusic0825 <tonymusic0825@gmail.com>
2026-04-07 15:14:29 +0900

    Added Scheduler Sort Logic
    - Using std::sort() from <algorithm>

--- process-scheduler/pscheduler.cpp
+++ process-scheduler/pscheduler.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <algorithm>
 using namespace std;
 
 class Task {
@@ -11,6 +12,10 @@ class Task {
     public:
         // Slightly faster way to initialize
         Task(string n, int p) : name(n), priority(p) {} 
+
+        int getPriority() const {
+            return priority;
+        }
 
         void display() const {
             cout << "PID: " << priority << ", " << "PName: " << name << '\n';
@@ -25,16 +30,20 @@ int main() {
     // Use emplace_back instead of push_back
     // Thsi constructs the 'Task' directly into memory instead of temp 'Task'
     tasks.emplace_back(Task("Fix Bug", 1));
-    tasks.emplace_back(Task("Fix Bug2", 2));
-    tasks.emplace_back(Task("Fix Bug3", 3));
-    tasks.emplace_back(Task("Fix Bug4", 4));
-    tasks.emplace_back(Task("Fix Bug5", 5));
+    tasks.emplace_back(Task("Fix Bug2", 42));
+    tasks.emplace_back(Task("Fix Bug3", 5612));
+    tasks.emplace_back(Task("Fix Bug4", 2));
+    tasks.emplace_back(Task("Fix Bug5", 10));
+
+
+    // Sort the tasks in priority order
+    sort(tasks.begin(), tasks.end(), [](const Task &a, const Task &b)
+        {return a.getPriority() < b.getPriority();});
 
     // Print all tasks
     // for (Task task : tasks) { // Create full copies of task...
     //     task.display();
     // }
-
     for (const Task &task : tasks) {
         task.display();
     }
