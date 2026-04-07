83b2dbe31e26e85cad84e81b12bdbfd897e53911
tonymusic0825 <tonymusic0825@gmail.com>
2026-04-07 18:32:49 +0900

    Made creating tree little more efficient

--- ast/mini_ast.cpp
+++ ast/mini_ast.cpp
@@ -35,20 +35,14 @@ int main() {
     auto root = make_unique<Node>("Add_Op");
 
     auto child1 = make_unique<Node>("Input_A");
-    auto child3 = make_unique<Node>("Input_A.2");
-    auto child4 = make_unique<Node>("Input_A.3");
+    child1->left = make_unique<Node>("Input_A.2");
+    child1->right = make_unique<Node>("Input_A.3");
 
     auto child2 = make_unique<Node>("Input_B");
-    auto child5 = make_unique<Node>("Input_B.2");
-    auto child6 = make_unique<Node>("Input_B.3");
+    child2->left = make_unique<Node>("Input_B.2");
+    child2->right = make_unique<Node>("Input_B.3");
 
     // Attach
-    child2->left = move(child5);
-    child2->right = move(child6);
-
-    child1->left = move(child3);
-    child1->right = move(child4);
-
     root->left = move(child1);
     root->right = move(child2);
 
