72ed99ec15d3032db3ecb76275c51b341e8213f8
tonymusic0825 <tonymusic0825@gmail.com>
2026-04-07 14:59:16 +0900

    Just simple PLAYGROUND things

--- playground/main.cpp
+++ playground/main.cpp
@@ -1,7 +1,50 @@
 #include <iostream>
+using namespace std;
+
+struct task {
+    int tid;
+    string tName;
+};
+
+class Employee {
+    private:
+        int salary;
+        int age;
+        int ssid;
+    
+    public:
+        string name;
+
+        Employee() {
+            salary = 0;
+            ssid = 0;
+            age = 0;
+            name = "Unknown";
+        }
+
+        Employee(int s, int a, int ss, string n) { 
+            name = n;
+            salary = s;
+            ssid = ss;
+            age = a;
+        }
+        
+        // Setters and Getters in public
+        void setSalary(int s) {
+            salary = s;
+        }
+
+        int getSalary() {
+            return salary;
+        }
+
+};
 
 int main() {
-    std::cout << "Hello World";
-    
+
+    Employee myObj;
+    myObj.setSalary(50000);
+    cout << myObj.getSalary();
+
     return 0;
 }
